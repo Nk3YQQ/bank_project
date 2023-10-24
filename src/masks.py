fb87d5de24cc5564f0020d88ddcfb0a8adbbc51b
Egor Nikitin <eganik5674@gmail.com>
2023-10-24 18:26:46 +0300

    update src/masks.py

--- src/masks.py
+++ src/masks.py
@@ -19,3 +19,29 @@ def mask_count(count_number: str) -> str:
 
     masked_count = "**" + count_number[16:20]
     return masked_count
+
+
+def mask_card_with_name(name, number: str | str) -> str | str:
+    """
+    Функция принимает на вход строку информацией тип карты/счета и номер карты/счета
+    и возвращает эту строку с замаскированным номером карты/счет
+    """
+    if len(number) == 16 and number.isdigit() is True:
+        masked_card = number[:4] + " " + number[4:6] + "** " + "**** " + number[12:]
+        return f'{name} {masked_card}'
+    elif len(number) == 20 and number.isdigit() is True:
+        masked_count = "**" + number[16:20]
+        return f'{name} {masked_count}'
+    else:
+        return 'Некорректный номер карты/счёта'
+
+
+def reformat_date(date_: str) -> str:
+    """
+    Функцию, которая принимает на вход строку, вида "2018-07-11T02:26:18.671407"
+    и возвращает строку с датой в виде "11.07.2018"
+    """
+    if 0 < int(date_[8:10]) <= 31 and 0 < int(date_[5:7]) <= 12:
+        return date_[8:10] + '.' + date_[5:7] + '.' + date_[0:4]
+    else:
+        return 'Некорректно введена дата'
