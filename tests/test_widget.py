b52072558960e29a05dc4932c01e7f7476f4fd9b
Egor Nikitin <eganik5674@gmail.com>
2023-10-30 19:57:22 +0300

    Update test_processing.py and test_widget.py

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,30 +1,50 @@
-from src.widget import mask_card_with_name, reformat_date, search_anagram, return_max_composition
+import pytest
+
+from src.widget import mask_card_with_name, reformat_date, return_max_composition, search_anagram
 
 
-def test_mask_card_with_name():
-    assert mask_card_with_name('Maestro', '7000792289606361') == 'Maestro 7000 79** **** 6361'
-    assert mask_card_with_name('Maestro', '70007922896063') == 'Некорректный номер карты/счёта'
-    assert mask_card_with_name('Счет', '64686473678894779589') == 'Счет **9589'
-    assert mask_card_with_name('Счет', '6468647367889477958922') == 'Некорректный номер карты/счёта'
-    assert mask_card_with_name('', '') == 'Некорректный номер карты/счёта'
+@pytest.mark.parametrize(
+    "name, number, expected_result",
+    [
+        ("Maestro", "7000792289606361", "Maestro 7000 79** **** 6361"),
+        ("Maestro", "70007922896063", "Некорректный номер карты/счёта"),
+        ("Счет", "64686473678894779589", "Счет **9589"),
+        ("Счет ", "3538303347444789556022", "Некорректный номер карты/счёта"),
+        ("", "", "Некорректный номер карты/счёта"),
+    ],
+)
+def test_mask_card_with_name(name: str, number: str, expected_result: str) -> None:
+    assert mask_card_with_name(name, number) == expected_result
 
 
-def test_reformat_date():
-    assert reformat_date('2018-07-11T02:26:18.671407') == '11.07.2018'
-    assert reformat_date('2023-10-31T02:26:18.671407') == '31.10.2023'
-    assert reformat_date('2023-13-31T02:26:18.671407') == 'Некорректно введена дата'
-    assert reformat_date('') == 'Некорректно введена дата'
-    assert reformat_date('2018-07-11') == '11.07.2018'
+@pytest.mark.parametrize(
+    "date_, expected_result",
+    [
+        ("2018-07-11T02:26:18.671407", "11.07.2018"),
+        ("2023-10-31T02:26:18.671407", "31.10.2023"),
+        ("2023-13-31T02:26:18.671407", "Некорректно введена дата"),
+        ("", "Некорректно введена дата"),
+        ("2018-07-11", "11.07.2018"),
+    ],
+)
+def test_reformat_date(date_: str, expected_result: str) -> None:
+    assert reformat_date(date_) == expected_result
 
 
-def test_search_anagram():
-    assert search_anagram(['hello', 'world', 'apple', 'pear', 'banana', 'pop']) == ['pop']
-    assert search_anagram(['madam', 'noon', 'level', '']) == ['madam', 'noon', 'level', '']
-    assert search_anagram([]) == []
+@pytest.mark.parametrize(
+    "list_, expected_result",
+    [
+        (["hello", "world", "apple", "pear", "banana", "pop"], ["pop"]),
+        (["madam", "noon", "level", ""], ["madam", "noon", "level", ""]),
+        ([], []),
+    ],
+)
+def test_search_anagram(list_: list[str], expected_result: list[str]) -> None:
+    assert search_anagram(list_) == expected_result
 
 
-def test_return_max_composition():
-    assert return_max_composition([2, 3, 5, 7, 11]) == 77
-    assert return_max_composition([-5, -7, -9, -13]) == 35
-    assert return_max_composition([1, 2]) == 2
-    assert return_max_composition([4]) == 0
+@pytest.mark.parametrize(
+    "list_, expected_result", [([2, 3, 5, 7, 11], 77), ([-5, -7, -9, -13], 35), ([1, 2], 2), ([4], 0)]
+)
+def test_return_max_composition(list_: list[int], expected_result: int) -> None:
+    assert return_max_composition(list_) == expected_result
